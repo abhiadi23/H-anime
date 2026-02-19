0892de0b5fe0a28e5b0daaed7796a6f681e3ecc1
•_• <abhinavaditya035@gmail.com>
2026-02-19 11:58:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 # CONFIG
 # ─────────────────────────────────────────────
 
-API_ID    = int(os.environ.get("API_ID", 0))
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID    = int(os.environ.get("API_ID", "27322718"))
+API_HASH  = os.environ.get("API_HASH", "4f6d1b67cf101aea5cf0536885aa1b82")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8564241159:AAGBkeIvZyzlrqyGY8_bTivOCWDKHgmLwgM")
